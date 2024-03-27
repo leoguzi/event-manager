4aa6805e67c8b6adcca486502c5f445815441c0d
Leonardo Guzi <leoguzi@hotmail.com>
2024-03-27 18:56:17 -0300

    Chore: creates the project structure

---
+++ pages/home/Home.tsx
@@ -0,0 +1,12 @@
+import styles from './styles';
+
+import { Text, View } from 'react-native';
+
+export default function HomeScreen() {
+    return (
+        <View style={styles.container} >
+            <Text>Home works! </Text>
+        </View>
+
+    );
+}
---
+++ pages/home/styles.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 60,
+    },
+});
+
+export default styles;
+
